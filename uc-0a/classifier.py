0ab09bf9a5512927d79d12ee49c80eef08a9a5a4
sravan13689 <sravan13689@gmail.com>
2026-05-07 17:10:27 +0530

    [UC-0a] modified classifier.py
    Earlier it was not executed now modified classifier.py and got the output results_pune.csv

--- uc-0a/classifier.py
+++ uc-0a/classifier.py
@@ -20,7 +20,6 @@ ALLOWED_CATEGORIES = [
     "Road Damage", "Heritage Damage", "Heat Hazard", "Drain Blockage", "Other"
 ]
 
-
 ALLOWED_PRIORITIES = ["Urgent", "Standard", "Low"]
 
 SEVERITY_KEYWORDS = [
